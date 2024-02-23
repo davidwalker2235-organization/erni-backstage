513c4cc9b14ad4e4ed5520ca454aeef73a9cb520
davidwalker2235 <davidwalker2235@gmail.com>
2024-02-23 12:08:02 +0100

    erfg

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
 # Environment, for example "prod" or "test"
 variable "environment" {
     type = string
-    default = "test"
+    default = "prod"
 }
 
 # The name of your new service, for example "backstage"
 variable "service_name" {
     type = string
-    default = "backstageerni"
+    default = "ernibackstage"
 }
 
 # Change this to match whatever suits you best
@@ -27,30 +27,17 @@ variable "db_admin_username" {
     default = "backstage"
 }
 
-# Now comes a bunch of the properties we mentioned when creating the Github application.
-/*
-variable "github_backstage_appid" {
+variable "github_organization" {
     type = string
-    default = "812384"
+    default = "davidwalker2235-organization"
 }
 
-variable "github_backstage_webhookUrl" {
+variable "github_backstage_appid" {
     type = string
+    default = "828233"
 }
 
 variable "github_backstage_clientId" {
     type = string
+    default = "Iv1.070a80f1762cab18"
 }
-
-variable "github_backstage_clientSecret" {
-    type = string
-}
-
-variable "github_backstage_webhookSecret" {
-    type = string
-}
-
-# If you're running this in Terraform Cloud, just replace all line breaks in the PEM file with \n and chain it all together to one long line.
-variable "github_backstage_privateKey" {
-    type = string
-}*/
